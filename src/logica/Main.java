77490baa763ad82447d3ca4b6dcc0ebf37a007df
Luis Molina <mixolydiannn@gmail.com>
2026-03-31 18:40:56 -0300

    login listo y usermenu wip

--- src/logica/Main.java
+++ src/logica/Main.java
@@ -9,9 +9,9 @@ public class Main {
 	//listas en paralelo xq no objetos allowed rip :<
 	
 	// Datos de Usuarios
-	static String[] listaNombres = new String[3];
-	static String[] listaPasswords = new String[3];
-	static int cantUsuarios = 3;
+	static String[] listaNombres = new String[10];
+	static String[] listaPasswords = new String[10];
+	static int cantUsuarios = 0;
 
 	// Datos de Registros (Maximo 300 segun el enunciado)
 	static String[] regUsuarios = new String[300];
@@ -21,45 +21,97 @@ public class Main {
 	static int cantRegistros = 0;
 	
 	public static void main(String[] args) throws FileNotFoundException {
-		
-		int choice = 0;
-		String uid;
-		String pw;
-		
-		do {
-			
-			System.out.println("1) Menu de Usuarios");
-			System.out.println("2) Menu de Analisis");
-			System.out.println("3) Salir");
-			
-			Scanner sc = new Scanner(System.in);
-			
-			choice = sc.nextInt();
-			
-			if (choice < 0 || choice > 3) {System.out.println("Ingrese un valor valido");System.out.println("");}
-			
-		}while (choice != 3);
-		
-		switch(choice) {
-		case 1 -> {
-			
-			//menuUsuarios();
-			
-			break;
-		}
-		case 2 -> {
-			
-			//menuAnalisis();
-			
-		}
-		}
-		
+	    
 		cargarUsuarios("usuarios.txt");
-		cargarRegistros("Registro.txt");
+		cargarRegistros("Registros.txt");
 		
+		
+	    Scanner sc = new Scanner(System.in); 
+	    
+	    int choice = 0;
+	    String uid;
+	    String pw;
+	    String usuarioon;
+	    
+	    do {
+	        System.out.println("1) Menu de Usuarios");
+	        System.out.println("2) Menu de Analisis");
+	        System.out.println("3) Salir");
+	        
+	        
+	        while (!sc.hasNextInt()) {
+	            System.out.println("Ingrese un valor numérico válido");
+	            sc.next(); // Limpiar el error
+	        }
+	        
+	        choice = sc.nextInt();
+	        sc.nextLine(); 
+
+	        if (choice < 1 || choice > 3) {
+	            System.out.println("Ingrese un valor válido");
+	        } else if (choice == 3) {
+	            break;
+	        }
+
+	        switch (choice) {
+	            case 1 -> {
+	                System.out.print("Usuario: ");
+	                uid = sc.nextLine(); 
+	                
+	                System.out.print("Contraseña: ");
+	                pw = sc.nextLine();
+	                
+	                usuarioon = validarLogin(listaNombres,listaPasswords,uid,pw);
+	                
+	                if (usuarioon != null) {
+	                	menuUsuarios(usuarioon);
+	                }
+	            }
+	            case 2 -> {
+	                //menuAnalisis();
+	            }
+	        }
+
+	    } while (choice != 3);
+
+	    System.out.println("Saliendo del programa...");
+	    //sc.close(); 
 	}
 	
 	
+	private static void menuUsuarios(String currentuser) {
+		Scanner menuU = new Scanner(System.in);
+		int useropt = 0;
+		
+		System.out.println("Acceso correcto !");
+		System.out.println(". . . . . . . . . . . .");
+		System.out.println("Bienvenido " + currentuser + " !");
+		System.out.println();
+		
+		do {
+			System.out.println("Que deseas realizar ?");
+			System.out.println();
+			System.out.println("1) Registrar actividad.");
+			System.out.println("2) Modificar actividad.");
+			System.out.println("3) Eliminar actividad.");
+			System.out.println("4) Cambiar contraseña.");
+			System.out.println("5) Salir.");
+			
+			useropt = menuU.nextInt();
+			
+			if (useropt < 1 || useropt > 5) {
+				System.out.println("Ingrese un valor valido. Reintentando . . .");
+				System.out.println();
+			}
+			
+		}while(useropt != 5);
+		
+		
+		
+		
+		
+	}
+
 	public static void cargarUsuarios(String ruta) throws FileNotFoundException {
 	    File archivo = new File(ruta);
 	    Scanner sc = new Scanner(archivo);
@@ -73,7 +125,7 @@ public class Main {
 	        String[] partes = linea.split(";");
 	        listaNombres[cantUsuarios] = partes[0];   
 	        listaPasswords[cantUsuarios] = partes[1]; 
-	        //cantUsuarios++; wait
+	        cantUsuarios++;
 	    }
 	    sc.close();
 	}
@@ -92,16 +144,15 @@ public class Main {
 	        regFechas[cantRegistros] = partes[1];
 	        regHoras[cantRegistros] = Integer.parseInt(partes[2]);
 	        regActividades[cantRegistros] = partes[3];
-	        
 	        cantRegistros++;
 	    }
 	    sc.close();
 	}
 	
-	public static String validarLogin(String[] nombres, String[] passwords, int cantUsuarios, String userTyped, String passTyped) {
+	public static String validarLogin(String[] nombres, String[] passwords, String userTyped, String passTyped) {
 	    
 	    // Recorremos los arreglos
-	    for (int i = 0; i < cantUsuarios; i++) {
+	    for (int i = 0; i < 3; i++) {
 	        
 	        
 	        if (nombres[i].equals(userTyped)) {
